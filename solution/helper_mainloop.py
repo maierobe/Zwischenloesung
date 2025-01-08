22d4134702fc03b7c588991cb405b18842af2871
quentinko <153619209+quentinko@users.noreply.github.com>
2025-01-09 00:28:34 +0100

    Update helper_mainloop.py

--- solution/helper_mainloop.py
+++ solution/helper_mainloop.py
@@ -11,21 +11,3 @@ def ensure_folder_exists(file_path):
         print(f"Folder created: {folder_path}")
     return
 
-def read_csv_file(path_csv):
-    csv_dataframe = pd.read_csv(path_csv)
-    #Zugreifen auf die einzelnen tasks mit z.B.: csv_dataframe.iloc[0], csv_dataframe.iloc[1], ...
-    # --> in jedem element dann 'path' und 'gripper'
-    # --> csv_dataframe.iloc[0]['path']
-    # --> csv_dataframe.iloc[0]['gripper']
-    return csv_dataframe
-
-def save_results(path_results, parts_list, d):
-    # save results as csv, visualisation plot and warnings/exceptions
-    path_csv = path_results
-    path_results_folder = os.path.dirname(path_results)
-    
-
-    # TODO: Diese funktion schreiben und in Main-Loop einbetten
-
-
-    return
